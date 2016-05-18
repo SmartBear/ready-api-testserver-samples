323a990cf7ae3c05881eb7d95b87f5fa1aa34cd7
Ole Lensmar <ole.lensmar@smartbear.com>
2016-05-18 14:26:53 -0400

    fixed dependencies

--- java/junit-client-sample/src/test/java/com/smartbear/readyapi/SimpleTest.java
+++ java/junit-client-sample/src/test/java/com/smartbear/readyapi/SimpleTest.java
@@ -3,27 +3,14 @@ package com.smartbear.readyapi;
 import com.smartbear.readyapi.client.TestRecipe;
 import com.smartbear.readyapi.client.execution.Execution;
 import com.smartbear.readyapi.client.model.ProjectResultReport;
-import com.smartbear.readyapi.client.teststeps.propertytransfer.PathLanguage;
-import com.smartbear.readyapi.client.teststeps.propertytransfer.PropertyTransferBuilder;
-import com.smartbear.readyapi.client.teststeps.propertytransfer.PropertyTransferSourceBuilder;
-import com.smartbear.readyapi.client.teststeps.propertytransfer.PropertyTransferTargetBuilder;
 import org.junit.Test;
 
+import java.net.URL;
 import java.util.Arrays;
-import java.util.regex.Matcher;
 
 import static com.smartbear.readyapi.client.TestRecipeBuilder.newTestRecipe;
-import static com.smartbear.readyapi.client.assertions.Assertions.jsonPathContent;
-import static com.smartbear.readyapi.client.assertions.Assertions.jsonPathCount;
-import static com.smartbear.readyapi.client.assertions.Assertions.validStatusCodes;
 import static com.smartbear.readyapi.client.teststeps.TestSteps.getRequest;
-import static com.smartbear.readyapi.client.teststeps.TestSteps.groovyScriptStep;
-import static com.smartbear.readyapi.client.teststeps.TestSteps.postRequest;
-import static com.smartbear.readyapi.client.teststeps.TestSteps.propertyTransfer;
-import static com.smartbear.readyapi.client.teststeps.TestSteps.restRequest;
 import static com.smartbear.readyapi.client.teststeps.TestSteps.soapRequest;
-import static javax.json.Json.createArrayBuilder;
-import static javax.json.Json.createObjectBuilder;
 import static org.junit.Assert.assertEquals;
 
 public class SimpleTest extends ApiTestBase
@@ -63,7 +50,7 @@ public class SimpleTest extends ApiTestBase
     @Test
     public void simpleSoapTest() throws Exception {
         TestRecipe recipe = newTestRecipe()
-            .addStep(soapRequest("http://www.webservicex.com/globalweather.asmx?WSDL")
+            .addStep(soapRequest( new URL("http://www.webservicex.com/globalweather.asmx?WSDL"))
                 .named("Soap Rulez")
                 .forBinding("GlobalWeatherSoap12")
                 .forOperation("GetWeather")
@@ -76,5 +63,7 @@ public class SimpleTest extends ApiTestBase
             .buildTestRecipe();
 
         executeAndAssert(recipe);
+
+
     }
 }
